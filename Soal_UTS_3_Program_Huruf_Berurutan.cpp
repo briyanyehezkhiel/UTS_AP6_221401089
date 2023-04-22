63f8a51c220318cb1af8adfec5cac4dec0c8dd76
briyanyehezkhiel <114645700+briyanyehezkhiel@users.noreply.github.com>
2023-04-22 14:04:14 +0700

    Menambah komentar di soal ke 3

--- Soal_UTS_3_Program_Huruf_Berurutan.cpp
+++ Soal_UTS_3_Program_Huruf_Berurutan.cpp
@@ -1,42 +1,54 @@
-/*Judul     : Soal UTS 3 "Program Huruf berurutan "
-  Nama      : Briyan Yehezkhiel
-  NIM       : 221401089
-  KOM       : C
-  Lab       : 6*/
+/*Judul : Soal UTS 3 "Program Huruf berurutan "
+Nama : Briyan Yehezkhiel
+NIM : 221401089
+KOM : C
+Lab : 6*/
 
+// Inklusi pustaka input/output stream pada bahasa C++
 #include <iostream>
+
+// Menggunakan namespace std agar tidak perlu menuliskan "std::" pada setiap pemanggilan fungsi
 using namespace std;
 
+// Fungsi utama
 int main() {
-    int n;
-    char ch = 'A';
-    cout << "Masukkan angka: ";
-    cin >> n;
-    if (n > 0 && n <= 26)
-    {
-        for (int i = n; i >= 1; i--) {
-            if (i == n) {  // jika i sama dengan n (iterasi pertama)
-                for (int j = 2; j <= i; j++) {
-                    cout << ch++;
-                }
-                for (int j = 1; j <= i; j++) {
-                    cout << ch--;
-                } 
-                ch--;
-            } else {  // selain iterasi pertama
-                for (int j = 1; j <= i; j++) {
-                    cout << ch++;
-                }
-                ch--;
-                for (int j = 1; j <= (n - i) * 2 - 1 ; j++) {
-                    cout << " ";
-                }
-                for (int j = 1; j <= i; j++) {
-                    cout << ch--;
-                }
+int n; // deklarasi variabel untuk menyimpan jumlah baris
+char ch = 'A'; // deklarasi variabel untuk menyimpan huruf yang akan ditampilkan
+cout << "Masukkan angka: "; // menampilkan pesan untuk meminta input dari user
+cin >> n; // meminta input dari user untuk jumlah baris
+
+// melakukan pengecekan apakah n berada dalam rentang 1-26
+if (n > 0 && n <= 26)
+{
+    // melakukan pengulangan sebanyak n kali, dimulai dari n dan decrement setiap iterasi
+    for (int i = n; i >= 1; i--) {
+        if (i == n) {  // jika i sama dengan n (iterasi pertama)
+            // melakukan pengulangan sebanyak 2 - i kali dan menampilkan huruf yang berurutan
+            for (int j = 2; j <= i; j++) {
+                cout << ch++;
             }
-            ch = 'A';
-            cout << endl;
+            // melakukan pengulangan sebanyak i kali dan menampilkan huruf yang berurutan (mulai dari huruf terakhir pada pengulangan sebelumnya)
+            for (int j = 1; j <= i; j++) {
+                cout << ch--;
+            } 
+            ch--; // mengembalikan nilai ch ke nilai sebelumnya
+        } else {  // selain iterasi pertama
+            // melakukan pengulangan sebanyak i kali dan menampilkan huruf yang berurutan
+            for (int j = 1; j <= i; j++) {
+                cout << ch++;
+            }
+            ch--; // mengurangi nilai ch sebanyak 1
+            // melakukan pengulangan sebanyak (n - i) * 2 - 1 kali dan menampilkan spasi
+            for (int j = 1; j <= (n - i) * 2 - 1 ; j++) {
+                cout << " ";
+            }
+            // melakukan pengulangan sebanyak i kali dan menampilkan huruf yang berurutan (mulai dari huruf terakhir pada pengulangan sebelumnya)
+            for (int j = 1; j <= i; j++) {
+                cout << ch--;
+            }
         }
+        ch = 'A'; // mengembalikan nilai ch ke 'A'
+        cout << endl; // menampilkan karakter baris baru
     }
+}
 }
